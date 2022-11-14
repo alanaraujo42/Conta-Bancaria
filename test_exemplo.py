b71929d0d3c2d2c238e1bea482fcb5ea23648f4e
Alan Araujo <alanaraujodev0@gmail.com>
2022-11-14 10:00:10 -0300

    de novo teste ci

---
+++ test_exemplo.py
@@ -0,0 +1,13 @@
+def test_soma(num=1, num2=1):
+    soma = num + num2
+    assert num + num2 == soma
+
+
+def soma(n, n2):
+    soma = n + n2
+    return soma
+
+
+print(soma(1, 1))
+print('Olá Mundo')
+print('Um mais um é igual a 2')
