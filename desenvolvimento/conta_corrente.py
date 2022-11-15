216170de4ec177a4e0ae05274359b48c3b08ff31
Alan Araujo <alanaraujodev0@gmail.com>
2022-11-15 09:36:38 -0300

    criação de conta_corrente e teste do mesmo

---
+++ desenvolvimento/conta_corrente.py
@@ -0,0 +1,9 @@
+class ContaCorrente:
+
+    def __init__(self, nome=str, cpf=int, idade=int, senha=str) -> None:
+        self.nome = nome
+        self.cpf = cpf
+        self.idade = idade
+        self.senha = senha
+        self.agencia = '0001'
+        self.numero_conta = '12345'
---
+++ test/test_conta_corrente.py
@@ -0,0 +1,35 @@
+import pytest
+from desenvolvimento.conta_corrente import ContaCorrente
+
+
+@pytest.fixture
+def conta():
+    return ContaCorrente('Alan Araújo Santos', '11122233344', 20, '1234')
+
+
+def test_cadastro_nome(conta):
+    assert isinstance(conta.nome, str)
+
+
+def test_cadastro_cpf(conta):
+    assert conta.cpf.isnumeric() and len(conta.cpf) == 11
+
+
+def test_cadastr_idade(conta):
+    assert isinstance(conta.idade, int) and conta.idade >= 18
+
+
+def test_cadastrar_senha(conta):
+    assert isinstance(conta.senha, str) and len(conta.senha) == 4
+
+
+def test_cadastro_agencia(conta):
+    assert isinstance(conta.agencia, str) and len(conta.agencia) == 4
+
+
+def test_cadastro_numero_conta(conta):
+    assert isinstance(conta.numero_conta, str) and len(conta.numero_conta) == 5
+
+
+def test_conta(conta):
+    assert isinstance(conta, ContaCorrente)
