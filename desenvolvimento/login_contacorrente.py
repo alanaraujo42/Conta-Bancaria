c7c70759aaecc57ca42dff48fc860396f1407d7c
Alan Araujo <alanaraujodev0@gmail.com>
2022-11-15 11:37:36 -0300

    add feature de login da conta corrente

---
+++ desenvolvimento/login_contacorrente.py
@@ -0,0 +1,42 @@
+from banco_de_dados import banco_de_contas
+
+
+def login_cpf():
+    while True:
+        cpf = input('Digite o seu CPF (somente números): ')
+        # Tratamento do dado
+        cpf = cpf.strip()
+        # Verificação do dado
+        if cpf.isnumeric() and len(cpf) == 11:
+            return cpf
+        else:
+            print('ERRO: CPF Inválido!')
+
+
+def login_senha():
+    while True:
+        senha = input('Digite uma senha com 4 dígitos: ')
+        senha = senha.strip()
+        if len(senha) == 4 and senha.isnumeric():
+            return senha
+        else:
+            print('ERRO: Somente 4 dígitos e números')
+
+
+def verificação_banco_de_dados(cpf, senha):
+    for conta in banco_de_contas:
+        dado_cpf = conta.cpf
+        dado_senha = conta.senha
+        if cpf == dado_cpf and senha == dado_senha:
+            print(conta)
+            return conta
+    return 'Você não possui cadastro!'
+
+
+def login():
+    # Input dos dados de login
+    cpf = login_cpf()
+    senha = login_senha()
+    # Verificação dos dados no banco de dados
+    resultado = verificação_banco_de_dados(cpf, senha)
+    return resultado
