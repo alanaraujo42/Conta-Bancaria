9f463fe08932e4101f70d188156d5b91ca3e084c
Alan Araujo <alanaraujodev0@gmail.com>
2022-11-16 17:15:35 -0300

    add metodos "depositar" e "sacar" dinheiro

--- desenvolvimento/conta_corrente.py
+++ desenvolvimento/conta_corrente.py
@@ -6,3 +6,24 @@ class ContaCorrente:
         self.senha = senha
         self.agencia = '0001'
         self.numero_conta = '12345'
+        self.saldo = 0
+
+    def depositar_dinheiro(self, valor):
+        self.saldo += valor
+
+    def verificar_saldo(self, valor):
+        if valor <= self.saldo:
+            return True
+        else:
+            return False
+
+    def sacar_dinheiro(self, valor):
+        """"
+        Função que vai verificar o saldo da conta e realizar saque, se valor menor ou igual 
+        ao saldo da e retornar True, caso contrátrio, False.
+        """
+        if ContaCorrente.verificar_saldo(valor):
+            self.saldo -= valor
+            return True
+        else:
+            return False
